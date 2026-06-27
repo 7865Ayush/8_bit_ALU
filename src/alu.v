63ea6f44b7ccd559b7b2437ef304450b909828af
7865Ayush <chakrabortyayush2017@gmail.com>
2026-06-27 19:51:27 +0530

    Implement addition operation

---
+++ src/alu.v
@@ -0,0 +1,37 @@
+module alu (
+    input  [7:0] A,
+    input  [7:0] B,
+    input  [3:0] ALU_Sel,
+
+    output reg [7:0] ALU_Out,
+    output reg CarryOut,
+    output reg Overflow,
+    output Zero,
+    output Negative
+);
+
+always @(*) begin
+
+    ALU_Out = 8'b00000000;
+    CarryOut = 1'b0;
+    Overflow = 1'b0;
+
+    case(ALU_Sel)
+
+    4'b0000: begin
+    {CarryOut, ALU_Out} = A + B;
+
+    Overflow = (~(A[7] ^ B[7])) & (A[7] ^ ALU_Out[7]);
+    end
+
+    default: begin
+    ALU_Out = 8'b00000000;
+    end
+
+    endcase
+end
+
+assign Zero = (ALU_Out == 8'b00000000);
+assign Negative = ALU_Out[7];
+
+endmodule
